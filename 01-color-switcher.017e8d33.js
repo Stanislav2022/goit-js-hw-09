ea2def67874afb001e570e13bc2ce4cb66f7dc1d
Stanislav2022 <tsv5@ukr.net>
2022-08-11 20:39:42 +0300

    1

--- 01-color-switcher.017e8d33.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),r=document.querySelector("body");e.setAttribute("disabled",!0),t.addEventListener("click",(()=>{timerId=setInterval((()=>{r.style.background=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),t.setAttribute("disabled",!0),e.removeAttribute("disabled")})),e.addEventListener("click",(()=>{clearInterval(timerId),t.removeAttribute("disabled"),e.setAttribute("disabled",!0)}));
-//# sourceMappingURL=01-color-switcher.017e8d33.js.map
---
+++ src/js/02-timer.js
@@ -0,0 +1,72 @@
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const btn = document.querySelector('[data-start]');
+const valueDays = document.querySelector('[data-days]');
+const valueHours = document.querySelector('[data-hours]');
+const valueMinutes = document.querySelector('[data-minutes]');
+const valueSeconds = document.querySelector('[data-seconds]');
+
+const timerEl = document.querySelector('.timer');
+const fieldsEl = document.querySelectorAll('.field');
+timerEl.style.cssText = "color: red; display: flex; font-size: 20px; padding: 10px";
+for (const field of fieldsEl) {
+    field.style.display = 'flex';
+    field.style.flexDirection = 'column';
+    field.style.alignItems = 'center';
+    field.style.padding = "15px"
+}
+
+btn.setAttribute('disabled', true);
+const today = new Date()
+const delta = 0;
+
+const options = {
+    enableTime: true,
+    time_24hr: true,
+    defaultDate: new Date(),
+    minuteIncrement: 1,
+    onClose(selectedDates) {
+      const chooseDate = selectedDates[0]
+       if(chooseDate < today) {
+        Notify.failure('Please choose a date in the future');
+        return
+      }
+      btn.removeAttribute('disabled')
+      btn.addEventListener('click', () => {
+        timerId = setInterval(() => {
+            const today = new Date();
+            const delta = chooseDate - today;
+            if (delta < 1000) {
+                clearInterval(timerId)
+            }
+            convertMs(delta)
+        }, 1000);
+      });
+    },
+   };
+  flatpickr("#datetime-picker", options);
+  
+  function convertMs(ms) {
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+   
+    const days = addLeadingZero(Math.floor(ms / day));
+    valueDays.textContent = days;
+    const hours = addLeadingZero(Math.floor((ms % day) / hour));
+    valueHours.textContent = hours;
+    const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+    valueMinutes.textContent = minutes;
+    const seconds = addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
+    valueSeconds.textContent = seconds;
+    return { days, hours, minutes, seconds };
+  }
+
+  function addLeadingZero(value) {
+    return String(value).padStart(2, '0');
+  }
+  
+
